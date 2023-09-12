200b5e231447a18fbcc7f6800501064893417314
jg-ribeiro <j.gabrielr@outlook.com>
2023-09-12 11:05:36 -0300

    test commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 
 /**
  * This activity allows the user to roll a dice and view the result on the screen
